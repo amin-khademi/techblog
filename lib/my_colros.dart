cae3654c2ba8cc0b2b8a183aa19783fce80de9d3
amin-khademi <amin.khademi2003@gmail.com>
2023-06-06 17:57:55 +0330

    flutter gen

---
+++ lib/my_colros.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SolidColor {
+  static const Color titile = Color.fromARGB(255, 40, 107, 184);
+  static const Color textTitle = Color.fromARGB(255, 0, 0, 0);
+  static const Color scafoldBg = Color.fromARGB(255, 255, 255, 255);
+}
+
+class GradientColors {
+  static const List<Color> bottomNavigator = [Color.fromARGB(255, 25, 0, 94),Color.fromARGB(255, 68, 4, 87)];
+}
---
+++ lib/my_strings.dart
@@ -0,0 +1,8 @@
+class MyString {
+  static const String viewHotestBlog = "مشاهده داغ ترین نوشته ها ";
+  static const String viewHotestPodcasts = "مشاهده داغ ترین پادکست ها ";
+  static const String relatedArticle = "نوشته های مرتبط";
+  static const String submitArticle = "ارسال مقاله";
+  static const String submitPodcasts = "ارسال پادکست";
+
+}
