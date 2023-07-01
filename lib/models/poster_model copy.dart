f6c8c8660635e8904460bd556b00881a6587a9aa
amin-khademi <amin.khademi2003@gmail.com>
2023-07-02 02:45:39 +0330

    http packages added to project

--- lib/models/poster_model copy.dart
+++ lib/models/poster_model copy.dart
@@ -1,23 +1,15 @@
-import 'dart:convert';
 
-import 'package:flutter/material.dart';
 
 class TagsModel {
   String? id;
   String? title;
 
- 
-
   TagsModel({
     required this.id,
     required this.title,
-
-  
-
   });
-  TagsModel.fromjsom(Map<String,dynamic>element ){
-  id=element["id"];
-  title=element["title"];
- 
- 
-  }}
+  TagsModel.fromjsom(Map<String, dynamic> element) {
+    id = element["id"];
+    title = element["title"];
+  }
+}
